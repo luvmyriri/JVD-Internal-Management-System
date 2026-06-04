294f78bbba7700bab978949023c069e9502cb8e2
LAMSEN <vjlamsenlamsen28@gmail.com>
2026-06-04 13:59:53 +0800

    Back

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -14,6 +14,11 @@ export default function Dashboard() {
     case 'super_admin':
     case 'executive_vice_president':
     case 'operations_manager':
+    case 'logistics_in_charge':
+    case 'dispatcher':
+    case 'purchasing_manager':
+    case 'service_adviser':
+    case 'head_mechanic':
       return <AdminDashboard />;
     case 'corporate_secretary':
       return <HRDashboard />;
